55971a1eddc5a39e01fbfb75c4a321b13cd041a2
RazielHernandez <razo.puma.azteca@gmail.com>
2026-01-14 12:10:01 -0500

    Deprecated code removed

--- functions/src/normalizers/teamPlayersNormalizer.ts
+++ functions/src/normalizers/teamPlayersNormalizer.ts
@@ -1,29 +1,3 @@
-// import {NormalizedPlayer} from "../types/player";
-// import {normalizePlayerStats} from "./playerStatsNormalizer";
-
-// /**
-//  * Normalizes a team player from API-Football.
-//  *
-//  * @param {any} item Raw API-Football player object.
-//  * @return {Player} Normalized player.
-//  */
-// export function normalizeTeamPlayer(item: any): NormalizedPlayer {
-//   if (!item || !item.player) {
-//     throw new Error("Invalid player response");
-//   }
-
-//   return {
-//     id: item.player.id,
-//     name: item.player.name,
-//     age: item.player.age,
-//     nationality: item.player.nationality,
-//     photo: item.player.photo,
-
-//     stats: Array.isArray(item.statistics) ?
-//       item.statistics.map(normalizePlayerStats) : [],
-//   };
-// }
-
 import {
   PlayerWithSeasonStats,
   PlayerSeasonStats,
